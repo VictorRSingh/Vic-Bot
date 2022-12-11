6ebd20023863de76f2427f68b441d37645778224
Victor Singh <vsingh57@myseneca.ca>
2022-12-11 00:36:42 -0500

    Fixed scheduler, added birthday.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,10 +1,30 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
+const fs = require('fs');
 
 module.exports = {
-    name: 'ready',
-    once: 'true',
-    async execute(interaction, client) {
-        const bot = client.user.tag;
-        console.log(chalk.green(`\nReady ` + chalk.blue(bot) +  ` is logged in and online.\n`));
+  name: "ready",
+  once: "true",
+  async execute(interaction, client) {
+    const bot = client.user.tag;
+    console.log(
+      chalk.green(`\nReady ` + chalk.blue(bot) + ` is logged in and online.`)
+    );
+
+    const eventFolder = fs.readdirSync(`./src/events`);
+    for(const folder of eventFolder) {
+        const eventFiles = fs
+        .readdirSync(`./src/events/${folder}`)
+        .filter(file => file.endsWith(".js"));
+
+      switch (folder) {
+        case "scheduler":
+          for (const file of eventFiles) {
+            const task = require(`../${folder}/${file}`);
+            task.execute(client);
+            console.log(chalk.green(`\nScheduling ` + chalk.blue(file) + ` to be executed.\n`));
+          }
+      }
     }
-}
+  },
+};
+
--- src/functions/handlers/handleScheduler.js
+++ src/functions/handlers/handleScheduler.js
@@ -2,23 +2,5 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 module.exports = (client) => {
-  client.handleScheduler = async () => {
-    const eventFolder = fs.readdirSync(`./src/scheduler`);
-    for (const folder of eventFolder) {
-      const schedulerFiles = fs
-        .readdirSync(`./src/scheduler/${folder}`)
-        .filter((file) => file.endsWith(".js"));
-
-      switch (folder) {
-        case "tasks":
-          for (const file of schedulerFiles) {
-            const task = require(`../../scheduler/${folder}/${file}`);
-            client.on(task.name, (...args) => 
-            task.execute(...args, client)
-            );
-          }
-
-      }
-    }
-  };
+  
 };
