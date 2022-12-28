e71328c0dda095a89fd1644dc8d97600e0b5d245
Victor Singh <vsingh57@myseneca.ca>
2022-12-28 16:55:58 -0500

    Changed website MainNav

--- src/website/components/MainNav.js
+++ src/website/components/MainNav.js
@@ -1,22 +1,35 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useRouter } from "next/router";
 
 function BasicExample() {
+  const router = useRouter();
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Victor Singh</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Victor Singh</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link href="/" passHref>
+                <Nav.Link active={router.pathname === "/"}>Home</Nav.Link>
+              </Link>
+              <Link href="/projects" passHref>
+                <Nav.Link active={router.pathname === "/projects"}>
+                  Projects
+                </Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
+    </>
   );
 }
 
---
+++ src/website/pages/projects.js
@@ -0,0 +1,7 @@
+export default function Projects(props) {
+    return(
+        <>
+            Projects will be listen here in the future.
+        </>
+    )
+}
