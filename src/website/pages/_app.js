a0d033d46f9a5ff144810f962dad6086fead5c88
Victor Singh <vsingh57@myseneca.ca>
2023-01-07 15:21:46 -0500

    Website changes

--- src/website/pages/_app.js
+++ src/website/pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="">
+      <div className="bg-cover h-screen bg-black/70">
         <MainNav />
         <Component {...pageProps} />
       </div>
