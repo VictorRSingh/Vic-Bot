54e1d3d730a6512ad4bbff8033c09a27fbea8c32
Victor Singh <vsingh57@myseneca.ca>
2023-01-06 21:36:37 -0500

    Website Changes

--- src/website/pages/_app.js
+++ src/website/pages/_app.js
@@ -4,9 +4,8 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="bg-fixed bg-center bg-cover h-screen bg-neutral-800 z-[2]">
-      <MainNav />
-      <br/><br/><br/>
+      <div className="custom-img">
+        <MainNav />
         <Component {...pageProps} />
       </div>
     </>
