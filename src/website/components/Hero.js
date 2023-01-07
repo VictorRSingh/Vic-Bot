9f35743b7a347863c4c03831b52ad2a7f5f9177e
Victor Singh <vsingh57@myseneca.ca>
2023-01-06 21:52:15 -0500

    Website changes

--- src/website/components/Hero.js
+++ src/website/components/Hero.js
@@ -1,7 +1,7 @@
 
 export default function Hero({heading, message}) {
     return(<>
-        <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover">
+        <div className="flex items-center justify-center min-h-screen mb-12 bg-fixed bg-center bg-cover">
             {/* Overlay */}
             <div className="absolute top-0 left-0 bottom-0 right-0 bg-black/70 z-[2]"/>
             <div className="p-5 text-white z-[2] mt-[-5rem]">
